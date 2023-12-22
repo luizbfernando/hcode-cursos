3184b5db3c7fc5832b58bcca94aa5e0a00e8828f
Luiz <luizfernandobrogiato73@gmail.com>
2023-12-22 12:44:50 -0300

    Trabalhando com Json

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,21 @@ document.querySelector('#form-login').addEventListener('submit', e => {
     let email = document.querySelector('#email').value;
     let password = document.querySelector('#password').value;
 
-    console.log(email, password)
+    let json = {
+        email,
+        password
+        
+    }
+
+    console.log(json);
+
+    let stringJSON = JSON.stringify(json);
+
+    console.log(stringJSON);
+
+    let jsonParse = JSON.parse(stringJSON);
+
+    console.log (jsonParse.password);
+
 
 })
