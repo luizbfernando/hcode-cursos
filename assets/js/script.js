92be7a3a5b54ef0be3aa27e9dd8feed3b354919b
Luiz <luizfernandobrogiato73@gmail.com>
2023-12-22 12:19:42 -0300

    Entendendo eventos do javascrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ email = "glaucio@hcode.com.br"
 
 console.log("O seu email é: " + email);
 console.log(`O seu email é:  ${email}`);
+
+document.querySelector('#form-login').addEventListener('mouseleave', e => {
+    console.log('O mouse está fora do formulário!');
+});
+
+document.querySelector('#form-login').addEventListener('submit', e => {
+    e.preventDefault();
+
+    console.log('Formulário enviado, aqui vai o Ajax.');
+})
