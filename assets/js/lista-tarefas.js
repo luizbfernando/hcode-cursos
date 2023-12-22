6a698270cdf6882b888d95cb032097e78e27720a
Luiz <luizfernandobrogiato73@gmail.com>
2023-12-22 17:14:52 -0300

    Adicionando Elementos em um Array com push

--- assets/js/lista-tarefas.js
+++ assets/js/lista-tarefas.js
@@ -1,44 +1,47 @@
-let data = [{
-    id: 1,
-    title: "Estudar html"
-}, {
-    id: 2,
-    title: "Esduar css"
-}, {
-    id: 3,
-    title: "Estudar javascript"
-}, {
-    id: 4,
-    title: "Estudar PHP"
-}];
+let data = [];
 
-data.forEach(task => {
-    let li = document.createElement('li');
+function renderTodo() {
 
-    li.innerHTML = `
+    document.querySelector('.todo').innerHTML = '';
+
+    data.forEach(task => {
+        let li = document.createElement('li');
+
+        li.innerHTML = `
     
     <input type="checkbox" id="task-${task.id}">
     <label for="task-${task.id}">${task.title}</label>
     
     `;
 
-    li.querySelector('input').addEventListener("change", e => {
+        li.querySelector('input').addEventListener("change", e => {
 
-        if (e.target.checked) {
-            li.classList.add('complete');
-        } else {
-            li.classList.add('remove');
-        }
+            if (e.target.checked) {
+                li.classList.add('complete');
+            } else {
+                li.classList.add('complete');
+            }
 
+        });
+
+        document.querySelector('.todo').append(li);
     });
 
-    document.querySelector('.todo').append(li);
-});
+}
 
 document.querySelector('#new-task').addEventListener('keyup', e => {
 
-    if(e.key === 'Enter') {
-        
+    if (e.key === 'Enter') {
+        data.push({
+            id: data.length + 1,
+            title: e.target.value
+        });
+
+        e.target.value = "";
+
+        renderTodo();
     }
 
 })
+
+renderTodo();
