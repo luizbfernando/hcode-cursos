b739aac282434f5cfcd176d83679dd395c2758db
Luiz <luizfernandobrogiato73@gmail.com>
2023-12-22 13:56:24 -0300

    Laço de repetição for

---
+++ assets/js/tabuada.js
@@ -0,0 +1,29 @@
+
+
+let tabuada = document.querySelector("#tabuada tbody");
+
+let valorA = 99;
+
+tabuada.innerHTML = ''; 
+
+for (let valorB = 0; valorB <= 10; valorB++) {
+
+    let resultado = valorA * valorB;
+
+    let template =
+        `
+            <td>${valorA}</td>
+            <td>x</td>
+            <td>${valorB}</td>
+            <td>=</td>
+            <td>${resultado}</td>
+        
+            `;
+
+    let tr = document.createElement('tr');
+
+    tr.innerHTML = template;
+
+    tabuada.append(tr);
+
+}
