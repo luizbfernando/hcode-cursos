cc91847a613f8e43e2fdefc96e3b877d9a4e719b
Luiz <luizfernandobrogiato73@gmail.com>
2023-12-22 16:46:14 -0300

    For each

--- assets/js/lista-tarefas.js
+++ assets/js/lista-tarefas.js
@@ -1,12 +1,26 @@
 let data = [{
     id: 1,
-    title: "Estudar html" 
+    title: "Estudar html"
 }, {
     id: 2,
     title: "Esduar css"
 }, {
     id: 3,
     title: "Estudar javascript"
+}, {
+    id: 4,
+    title: "Estudar PHP"
 }];
 
-console.log(data)
+data.forEach(task => {
+    let li = document.createElement('li');
+
+    li.innerHTML = `
+    
+    <input type="checkbox" id="task-${task.id}">
+    <label for="task-${task.id}">${task.title}</label>
+    
+    `;
+
+    document.querySelector('.todo').append(li);
+});
